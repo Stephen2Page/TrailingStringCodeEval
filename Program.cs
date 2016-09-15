e6220dd3dd677531dcbfff50579696a31a5940fe
Stephen2Page <stephen2page@gmail.com>
2016-09-14 21:13:07 -0400

    initial commit
    There are two strings: A and B. Print 1 if string B occurs at the end of
    string A. Otherwise, print 0.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrailingStringCodeEval
+{
+    class Program
+    {
+        //There are two strings: A and B. 
+        //Print 1 if string B occurs at the end of string A. Otherwise, print 0.
+        //The first argument is a path to the input filename containing two comma-delimited strings, 
+        //one per line. Ignore all empty lines in the input file.
+
+
+        static void Main(string[] args)
+        {
+            string line = ("Hello CodeEval,CodeEval");
+            //Parse input string into last word and individual words in first part
+            //uses comma as separator
+            string[] splitString = line.Split(',');
+            string sentence = splitString[0];
+            //Check last word (end of array) against second string
+
+            //Print either 1 or 0
+            if word 
+
+
+        }
+    }
+}
